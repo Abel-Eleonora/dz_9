5c2796fed60612e53c0ea1788ab4f83411ece419
Abel-Eleonora <abeleleonora1@gmail.com>
2021-03-27 16:29:11 +0300

    to find errors

---
+++ src/com/company/Address.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Address {
+
+    private String city, street;
+    private int id;
+
+    public Address(String city, String street, int id) {
+        this.city = city;
+        this.street = street;
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
---
+++ src/com/company/PersonIOUtil.java
@@ -0,0 +1,55 @@
+package com.company;
+
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonIOUtil {
+
+    private String fileName;
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public static void writePerson (String fileName, List<Person> persons) throws IOException{
+        try (FileWriter writer = new FileWriter(fileName)){
+            writer.write(PersonCsvUtil.headers());
+            for (Person person : persons){
+                writer.write(PersonCsvUtil.toCsvString(person));
+            }
+
+        }
+    }
+
+
+
+    public static List<Person> readPerson (String fileName) throws IOException, EmptySourceFileException {
+
+        List<Person> persons = new ArrayList<>();
+        if (fileName == null){
+            throw new EmptySourceFileException("Файл не существует или он пустой");
+        }
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))){
+            String csvString;
+            while ((csvString = reader.readLine()) != null){
+                Person person = PersonCsvUtil.toObject(csvString);
+                persons.add(person);
+            }
+        } catch (CsvException e){
+            e.printStackTrace();
+        }
+        return persons;
+
+    }
+
+
+
+
+
+}
